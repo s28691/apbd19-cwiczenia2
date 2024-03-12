fda1f57e713f99b621914c4407b4bb6854fd26aa
s28691 <s28691@pjwstk.edu.pl>
2024-03-12 13:30:54 +0100

    Add GetAverage method

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,3 +3,13 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja");
 Console.WriteLine("Modyfikacja 2");
+
+static double GetAverage(int[] tablica)
+{
+ int suma = 0;
+ foreach (var number in tablica)
+ {
+  suma += number;
+ }
+ return (double) suma / tablica.Length;
+}
